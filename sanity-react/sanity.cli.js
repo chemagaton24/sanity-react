0ac510b597a6ae43f384cf323af83716994dffa6
chemagaton24 <chembeeragaton@gmail.com>
2024-02-29 21:36:37 +0800

    first commit

---
+++ sanity-react/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'ydfper6j',
+    dataset: 'production'
+  }
+})
---
+++ src/client.js
@@ -0,0 +1,6 @@
+import { createClient } from "@sanity/client";
+
+export default createClient({
+  projectId: "ydfper6j",
+  dataset: "production",
+});
